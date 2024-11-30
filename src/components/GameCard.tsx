d72a1850ba93d371ddc20dd116ee03d86c5f7b95
Husanboy Zafarov <115022906+HusanboyZafarov@users.noreply.github.com>
2024-12-01 00:33:52 +0500

    Get optimized images

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -4,11 +4,12 @@ import {
   CardBody,
   Heading,
   HStack,
-  Image,
-} from "@chakra-ui/react";
-import { Game } from "../hooks/useGames";
-import CriticScore from "./CriticScore";
-import PlatformIconList from "./PlatformIconList";
+  Image
+} from '@chakra-ui/react';
+import { Game } from '../hooks/useGames';
+import getCroppedImgUrl from '../services/img-url';
+import CriticScore from './CriticScore';
+import PlatformIconList from './PlatformIconList';
 
 interface Props {
   game: Game;
@@ -16,15 +17,11 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
-      <Image
-        src={game.background_image}
-        height={{ sm: 80, md: 60, lg: 60, xl: 40 }}
-        objectFit="cover"
-      />
+    <Card borderRadius={10} overflow='hidden' key={game.id}>
+      <Image src={getCroppedImgUrl(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between" mt={2}>
+        <Heading fontSize='2xl'>{game.name}</Heading>
+        <HStack justifyContent='space-between' mt={2}>
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -4,15 +4,15 @@ import {
   FaXbox,
   FaApple,
   FaLinux,
-  FaAndroid,
-} from "react-icons/fa";
+  FaAndroid
+} from 'react-icons/fa';
 
-import { MdPhoneIphone } from "react-icons/md";
-import { SiNintendo } from "react-icons/si";
-import { BsGlobe } from "react-icons/bs";
-import { Platform } from "../hooks/useGames";
-import { HStack, Icon } from "@chakra-ui/react";
-import { IconType } from "react-icons/lib/esm/iconBase";
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
+import { Platform } from '../hooks/useGames';
+import { HStack, Icon } from '@chakra-ui/react';
+import { IconType } from 'react-icons/lib/esm/iconBase';
 
 interface Props {
   platforms: Platform[];
@@ -28,13 +28,13 @@ const PlatformIconList = ({ platforms }: Props) => {
     linux: FaLinux,
     ios: MdPhoneIphone,
     android: FaAndroid,
-    web: BsGlobe,
+    web: BsGlobe
   };
 
   return (
     <HStack marginY={1}>
       {platforms.map((platform) => (
-        <Icon as={iconMap[platform.slug]} color="gray.500" />
+        <Icon as={iconMap[platform.slug]} key={platform.id} color='gray.500' />
       ))}
     </HStack>
   );
