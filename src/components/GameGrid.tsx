c4400346a9709d6984e1958418b30878f86e6d2c
Husanboy Zafarov <zhzr1222@gmail.com>
2024-12-04 17:29:33 +0500

    Build: Sort selector

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -36,7 +36,7 @@ const GameGrid = ({ gameQuery }: Props) => {
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         spacing={3}
-        marginTop={2}
+        marginTop={3}
       >
         {isLoading
           ? skeletons.map((skeleton) => (
