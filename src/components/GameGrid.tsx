f2b7ade0ace28d6b3002a928bb0007713e4db7c8
Husanboy Zafarov <zhzr1222@gmail.com>
2024-12-06 10:09:15 +0500

    Refactor: Game Grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -25,34 +25,32 @@ const GameGrid = ({ gameQuery }: Props) => {
     "0.1",
   ];
 
+  if (error)
+    return (
+      <Alert status="error" sx={{ borderRadius: "10px 0px 0px 10px" }}>
+        <AlertIcon />
+        <AlertTitle>{error}</AlertTitle>
+      </Alert>
+    );
+
   return (
-    <>
-      {error && (
-        <Alert status="error" sx={{ borderRadius: "10px 0px 0px 10px" }}>
-          <AlertIcon />
-          <AlertTitle>{error}</AlertTitle>
-        </Alert>
-      )}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
-        spacing={3}
-        marginTop={3}
-      >
-        {isLoading
-          ? skeletons.map((skeleton) => (
-              <GameCardContainer
-                key={parseFloat(skeleton) * Math.random() * 10}
-              >
-                <GameCardSkeleton opacity_num={skeleton} />{" "}
-              </GameCardContainer>
-            ))
-          : data.map((game) => (
-              <GameCardContainer key={game.id}>
-                <GameCard game={game}></GameCard>
-              </GameCardContainer>
-            ))}
-      </SimpleGrid>
-    </>
+    <SimpleGrid
+      columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+      spacing={3}
+      marginTop={3}
+    >
+      {isLoading
+        ? skeletons.map((skeleton) => (
+            <GameCardContainer key={parseFloat(skeleton) * Math.random() * 10}>
+              <GameCardSkeleton opacity_num={skeleton} />{" "}
+            </GameCardContainer>
+          ))
+        : data.map((game) => (
+            <GameCardContainer key={game.id}>
+              <GameCard game={game}></GameCard>
+            </GameCardContainer>
+          ))}
+    </SimpleGrid>
   );
 };
 
