da32802f17226f239cbae022cd6772a832ea471e
Husanboy Zafarov <zhzr1222@gmail.com>
2024-12-20 18:08:44 +0500

    Refactor: Entities

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,6 @@
 import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
-import { Game } from "../entities/Game";
+import  Game from "../entities/Game";
 import getCroppedImgUrl from "../services/img-url";
 import CriticScore from "./CriticScore";
 import Emoji from "./Emoji";
--- src/hooks/useScreenshoots.ts
+++ src/hooks/useScreenshoots.ts
@@ -1,10 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
-import { Screenshoot } from "../entities/Screenshoot";
+import Screenshoot from "../entities/Screenshoot";
 import APIClient from "../services/api-client";
 
 const useScreenshoots = (gameId: number) => {
   const apiClient = new APIClient<Screenshoot>(`/games/${gameId}/screenshoots`);
- 
+
   return useQuery({
     queryKey: ["screenshoots", gameId],
     queryFn: apiClient.getAll,
